74bbd67321b27f8c27f9abff0d9bc43e6b61f4bc
Abir-Bouhriz <bouhrizabir2015@gmail.com>
2021-01-07 20:12:46 +0100

    home content

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import About from '../views/About'
-import Activities from '../views/Activities'
-import News from '../views/News'
-import Contact from '../views/Contact'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -16,24 +13,11 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    component: About
-  },
-  {
-    path: '/activities',
-    name: 'Activities',
-    component: Activities
-  },
-  {
-    path: '/news',
-    name: 'News',
-    component: News
-  },
-  {
-    path: '/contact',
-    name: 'Contact',
-    component: Contact
-  },
-
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="home">
+    <v-row>
+      <v-col lg="2">
       <v-navigation-drawer width="370px" style="background:rgb(7,15,19);" absolute permanent>
         <template v-slot:prepend  >
           <img src="logo.png" height="155px" width="320px" class="pt-10 pl-10">
-            <div class="text-center grey--text mt-5" >
+            <div class="text-center mt-5" >
               <v-icon x-small class="grey--text">control_camera</v-icon>
-              <v-icon x-small class="grey--text">control_camera</v-icon>
-              <v-icon x-small class="grey--text">control_camera</v-icon>
+              <v-icon x-small class="grey--text ml-3">control_camera</v-icon>
+              <v-icon x-small class="grey--text ml-3">control_camera</v-icon>
 <br>
-              <p text class="grey--text mb-3">FR / EN</p>
+              <p text class="grey--text body-1 mt-4 mb-3">FR / EN</p>
 <br>
               <v-list dense class="text-center">
                 <v-list-item two-line v-for="item in items" :key="item.title">
                   <v-list-item-content>
-                    <v-list-item-title class="title grey--text my-2">
+                    <v-list-item-title class="body-1 grey--text my-2">
                       {{ item.title }}
                     </v-list-item-title>
                     <v-list-item-subtitle>
@@ -24,15 +26,30 @@
               </v-list>
 <br>
               <v-icon x-small class="grey--text">control_camera</v-icon>
-              <v-icon x-small class="grey--text">control_camera</v-icon>
-              <v-icon x-small class="grey--text">control_camera</v-icon>
+              <v-icon x-small class="grey--text ml-3">control_camera</v-icon>
+              <v-icon x-small class="grey--text ml-3">control_camera</v-icon>
 <br><br>
-              <p class="grey--text mt-5 caption">FOLLOW US</p>
+              <p class="grey--text mt-5 " style="font-size:0.6rem;">FOLLOW US</p>
               <img src="footer.png" height="50px" class="mb-3">
-              <p class="grey--text caption mx-7">© 2021, <a href="https://github.com/Abir-Bouhriz"  >BOUHRIZ ABIR</a> </p>
+              <p class="grey--text mx-16" style="font-size:0.6rem;">© 2021, SPECTRE - ALL RIGHTS RESERVED WORDPRESS BY COLORZ </p>
             </div>
         </template>
       </v-navigation-drawer>
+      </v-col>
+      <v-col lg="10" class="justify-center">
+        <v-img src="home-bg.jpg" height="820px"  >
+          <div id="content" class="text-center"  >
+            <h1 class="white--text">PREMIUM MUSIC AND <br> SOUND DESIGN</h1>
+            <p class="white--text mt-5 ">THE ART OF TRANSLATING YOUR IDENTITY INTO MUSIC</p>
+             <v-icon x-small class="white--text">control_camera</v-icon>
+              <v-icon x-small class="white--text ml-5">control_camera</v-icon>
+              <v-icon x-small class="white--text ml-5">control_camera</v-icon>
+            <p class="body-1 white--text mt-10">We are expert in Retail Music Marketing. Our artistic and technical turnkey solutions combined with our extensive knowledge of retail marketing make our company your best music partner to support all your in and out store trading-up strategies.</p>
+            <v-btn text class="px-10 py-5 white--text mt-10" style="border: 2px solid white;">LEARN MORE ABOUT US</v-btn>
+          </div>
+        </v-img>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -56,11 +73,16 @@ export default {
 </script>
 
 <style >
-.mt-n2 a{
-  text-decoration: none;
-  color: grey !important;
+#content{
+  margin-top: 220px;
 }
-.mt-n2  a:hover{
-  color: white !important;
+#content h1{
+  font-size: 2rem;
+  letter-spacing: 10px;
+}
+#content p{
+  font-size: 1rem;
+  margin-right: 400px;
+  margin-left: 400px;
 }
 </style>
